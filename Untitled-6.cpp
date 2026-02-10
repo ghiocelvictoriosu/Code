40a3d7a9aa6cfd52c8a5e397beed09e23a16e538
Toma <142803969+ghiocelvictoriosu@users.noreply.github.com>
2026-02-10 10:06:46 +0200

    Add files via upload

--- Untitled-6.cpp
+++ Untitled-6.cpp
@@ -1,54 +1,49 @@
 #include <iostream>
-#include <stdio.h>
 using namespace std;
 
-int main(){
-    
-    int x;
-    cout<<"Choose your first number: ";
-    cin>>x;
-    
-    int y;
-    cout<<"Choose your second number: ";
-    cin>>y;
-    
-    string sign;
-    int sum;
-    sum = x+y;
-    cout<<"Choose what you want to use: ";
-    cin>>sign;
-    
-    int product = x*y;
-    int subtraction = x-y;
-
-    if (sign == "+"){
-
-        cout<<""<<sum<<endl;
-
-    } else if (sign == "-"){
-
-        cout<<""<<subtraction<<endl;
-
-    } else if (sign == "*"){
-
-        cout<<""<<product<<endl;
-
-    } else if (sign == "/"){
-
-        if (y == 0){
-
-            cout<<"Cannot divide by 0"<<endl;
-            return 1;
-
-        } else {
-            
-            double divison = (double)x/y;
-            cout<<""<<divison<<endl;
-            
+double calculate(int x, int y, char op) {
+    if (op == '+') {
+        return x + y;
+    }
+    else if (op == '-') {
+        return x - y;
+    }
+    else if (op == '*') {
+        return x * y;
+    }
+    else if (op == '/') {
+        if (y == 0) {
+            cout << "Cannot divide by 0" << endl;
+            return 0;
         }
+        return (double)x / y;
+    }
+    else {
+        cout << "Invalid " << endl;
+        return 0;
+    }
+}
 
+int main() {
+    int x, y;
+    char op;
+    char again = 'y';
+
+    while (again == 'y') {
+        cout << "Choose your first number: ";
+        cin >> x;
+
+        cout << "Choose your second number: ";
+        cin >> y;
+
+        cout << "Choose operation (+ - * /): ";
+        cin >> op;
+
+        cout << "Result: " << calculate(x, y, op) << endl;
+
+        cout << "Do you want to calculate again? (y/n): ";
+        cin >> again;
     }
 
-return 0;
-
+    return 0;
 }
